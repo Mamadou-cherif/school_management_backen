e21d52ebe2d2fbbf4d3571b1787059d194322865
Haidara <alhaidara88@gmail.com>
2022-06-16 10:43:44 +0000

    H upload

--- routes/document.js
+++ routes/document.js
@@ -1,11 +1,14 @@
-const express=require("express")
-const router= express.Router()
-const documentController= require("../controllers/document")
+const express = require("express")
+const router = express.Router()
+const documentController = require("../controllers/document")
 
-    router.post("/addDocument", documentController.addDocument)
-    router.post("/documentSelectBy", documentController.documentSelectBy)
-    router.put("/disableDocument", documentController.disableDocument)
-    router.put("/updateDocument", documentController.updateDocument)
-    router.get("/selectByIdDocument/:id", documentController.selectByIdDocument)
 
-module.exports= router
+router.post("/documents", documentController.files)
+router.post("/addDocument", documentController.addDocument)
+router.post("/documentSelectBy", documentController.documentSelectBy)
+router.get("/getfile/:File", documentController.getImageFile)
+router.put("/disableDocument", documentController.disableDocument)
+router.put("/updateDocument", documentController.updateDocument)
+router.get("/selectByIdDocument/:id", documentController.selectByIdDocument)
+
+module.exports = router
